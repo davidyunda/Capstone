1ccdbaab81be5653765117d25497fdde3e320158
clara <10088152+claraj@users.noreply.github.com>
2022-02-13 11:14:24 -0600

    project feedback

--- Project1/quiz_program.py
+++ Project1/quiz_program.py
@@ -12,26 +12,58 @@ class Topic:
     def add(self, question):
         self.questions.append(question)
 
-    def __str__(self):
-        #loop in list
-        for a in self.questions:
-            # print(a)
-            answer = input(a[0])
-            #ignore case and check if answer is correct
-            if answer.lower() == a[1].lower():
-                print('Correct!\n')
-            else:
-                print('Sorry, the answer is ' + a[1] + '.\n')
+    # __str__ should be used to return a basic string representation of an object, you should'nt have logic here
+    # use a regular method to ask the questions
+    def ask_topic_questions(self):
 
-        return f'{self.topic}'
+        # keep track of score here
+        total_score = 0 
+        #loop in list
+        
+        for question_and_answer in self.questions:  # avoid single letter variable names - what is 'a' ?  One question and answer? 
+            # print(a)
+            answer = input(question_and_answer[0])
+            #ignore case and check if answer is correct
+            if answer.lower() == question_and_answer[1].lower():
+                print('Correct!\n')
+                total_score += 1
+            else:
+                print('Sorry, the answer is ' + question_and_answer[1] + '.\n')
+
+        # return f'{self.topic}'  # not needed
+        # so you can print the number of correct questions, and if the user got all questions correct 
+        print(f'Total correct answers = {total_score}')
+        if total_score == len(self.questions):
+            print('You got all questions correct!')
+            
 
 def main():
 
     #display welcome message
     print('\nQUIZ PROGRAM\n')
 
-    #all topics save in list
-    all_topics = ['art','space']
+    #create new topic and questions  - set this up first 
+    # use more descriptive variable names 
+    art_topic = Topic('art')
+
+    # consider storing each question as a dictionary so the data is clearly labeled with the keys
+    # it's easy to mix up list indexes. Accessing the data with 
+    # example:  { 'question': 'Who painted the Mona Lisa?', 'answer': 'Leonardo Da Vinci' }
+
+    
+    art_topic.add(['Who painted the Mona Lisa?', 'Leonardo Da Vinci'])
+    art_topic.add(['What precious stone is used to make the artist\'s pigment ultramarine?', 'Lapiz lazuli'])
+    art_topic.add(['Anish Kapoor\'s bean-shaped Cloud Gate scuplture is a landmark of which city?', 'Chicago'])
+    
+    space_topic = Topic('space')
+    space_topic.add(['Which planet is closest to the sun?', 'Mercury'])
+    space_topic.add(['Which planet spins in the opposite direction to all the others in the solar system?', 'Venus'])
+    space_topic.add(['How many moons does Mars have?', '2'])
+
+    #all topics save in list 
+    # all_topics = ['art','space']
+    # or a dictionary - topic string name and Topic objects 
+    all_topics = { 'art': art_topic, 'space': space_topic}
 
     #if the input from the user is not in the list it will ask again
     while True:
@@ -43,18 +75,14 @@ def main():
         else:
             print('Enter again\n')
 
-    #create new topic and questions 
-    new_topic = Topic('art')
-    new_topic.add(['Who painted the Mona Lisa?', 'Leonardo Da Vinci'])
-    new_topic.add(['What precious stone is used to make the artist\'s pigment ultramarine?', 'Lapiz lazuli'])
-    new_topic.add(['Anish Kapoor\'s bean-shaped Cloud Gate scuplture is a landmark of which city?', 'Chicago'])
-    
-    new_topic_2 = Topic('space')
-    new_topic_2.add(['Which planet is closest to the sun?', 'Mercury'])
-    new_topic_2.add(['Which planet spins in the opposite direction to all the others in the solar system?', 'Venus'])
-    new_topic_2.add(['How many moons does Mars have?', '2'])
-    
-    print(new_topic)
-    print(new_topic_2)
+    # now you have a topic and can ask just those questions, look up the Topic in the all_topics dictionary,
+
+    selected_topic_questions_and_answers = all_topics[topic]
+
+    selected_topic_questions_and_answers.ask_topic_questions()
+
+    # The user should be able to choose one topic from the choices 
+    # print(new_topic)
+    # print(new_topic_2)
 
 main()
