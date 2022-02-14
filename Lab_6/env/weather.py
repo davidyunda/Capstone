3be742a7b407225da1141f7cdf436755be0c97fe
davidyunda <nm5128kk@go.minnstate.edu>
2022-02-13 20:36:28 -0600

    lab_6

---
+++ Lab_6/env/weather.py
@@ -0,0 +1,8 @@
+from pprint import pprint
+import requests
+
+url = 'api.openweathermap.org/data/2.5/weather?q=minneapolis,us&units=imperial&appid=a4df1057eaab2c75de7de6fe62b89355'
+
+data = requests.get(url).json()
+
+pprint(data)
